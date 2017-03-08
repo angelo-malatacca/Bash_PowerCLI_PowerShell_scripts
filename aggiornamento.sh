243857985c115daeb93b934eb21da2c6e93894e5
angelo-malatacca <fallen619@hotmail.it>
2017-03-08 19:32:13 +0100

    primo upload

--- aggiornamento.sh
+++ aggiornamento.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
-sleep 2
+sleep 3
+echo ""
+echo ""
 echo "##########################"
 echo "# Verifica aggiornamenti #"
 echo "##########################"
+echo ""
+sleep 3
 sudo apt-get update
 echo ""
 echo ""
-sleep 2
-
+sleep 3
 echo "###############################"
 echo "# Installazione aggiornamenti #"
 echo "###############################"
+echo ""
+sleep 3
 sudo apt-get upgrade -y
 echo ""
 echo ""
-sleep 2
-
+sleep 3
 echo "#########################"
 echo "# Aggiornamento sistema #"
 echo "#########################"
+echo ""
+sleep 3
 sudo apt-get dist-upgrade -y
-
-sleep 2
-
+sleep 3
 echo "##############################"
 echo "# Pulizia file non necessari #"
 echo "##############################"
+echo ""
+sleep 3
 sudo apt-get autoremove -y
 echo ""
 echo ""
-sleep 2
+sleep 3
 
 exit
